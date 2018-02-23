052b52c9338d48c5acf4c46f8610f38f551a4af3
Mila Zangirolame <milazangirolame@gmail.com>
2018-02-23 00:39:08 -0300

    basic queries with objects

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -8,21 +8,34 @@ Types::QueryType = GraphQL::ObjectType.define do
     }
   end
 
-  # field :posts, Types::PostType do
-  #   argument :id, types.ID
-  #   description "id of the post you want details"
-  #   resolve -> (obj, args, ctx) {
-  #     Post.where(id: args[:id])
-  #   }
-  # end
+  field :post, Types::PostType do
+    argument :id, types.ID
+    description "id of the post you want details"
+    resolve -> (obj, args, ctx) {
+      Post.where(id: args[:id])
+    }
+  end
 
-  # field :posts, Types::PostType do
-  #   argument :user_id, types.ID
-  #   description "id of the author you ant to see posts"
-  #   resolve -> (obj, args, ctx) {
-  #     Post.where(user_id: args[:user_id])
-  #   }
-  # end
+  field :posts, Types::PostType do
+    argument :user_id, types.ID
+    description "id of the author you ant to see posts"
+    resolve -> (obj, args, ctx) {
+      Post.where(user_id: args[:user_id])
+    }
+  end
+
+  field :user, Types::UserType do
+    argument :posts, types.ID
+    description "all posts from this user"
+    resolve -> (obj, args, ctx) {
+      obj.posts.each do |post|
+        post.name
+        post.body
+        post.comments
+      end
+
+    }
+  end
 
 
 
