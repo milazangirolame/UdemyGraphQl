9aaefb5c44a6d008f3f5bd26ad1ec53d5c3a7071
Mila Zangirolame <milazangirolame@gmail.com>
2018-03-01 01:27:51 -0300

    fixed controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,21 +3,7 @@ class PagesController < ApplicationController
   def home
     @posts = Post.all
     @fields = Post.column_names
-
-
   end
-
-  private
-
-  def field_name(field)
-    field_name = Post.columns_hash['field'].name
-
-    def method(field)
-          return post.field_name
-
-      end
-
-    end
 
 
 end
